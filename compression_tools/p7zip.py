d6cad54e1bf12bcd60ea6f819fb25d909bd1b4a9
Allan Callaghan <calaldees@gmail.com>
2020-07-29 18:39:16 +0100

    7z wrapper now works and has tests

--- compression_tools/p7zip.py
+++ compression_tools/p7zip.py
@@ -13,9 +13,6 @@ class P7Zip():
 
     REGEX_HASH_SHA1 = re.compile(b'[A-Fa-f0-9]{40}')
 
-    def __init__(self):
-        pass
-
     def hash(self, source):
         """
         >>> P7Zip().hash('./test/test.txt')
@@ -33,26 +30,43 @@ class P7Zip():
 
     def compress(self, cwd, files, destination_file):
         """
-        files relative to cwd
+        >>> import tempfile
+        >>> temp_directory1 = tempfile.TemporaryDirectory()
+        >>> compressed_file = os.path.join(temp_directory1.name, 'test.7z')
+        >>> P7Zip().compress(
+        ...     cwd='./',
+        ...     files=('test/test.txt',),
+        ...     destination_file=compressed_file
+        ... )
+        >>> temp_directory2 = tempfile.TemporaryDirectory()
+        >>> P7Zip().extract(
+        ...     cwd=temp_directory2.name,
+        ...     source_file=compressed_file,
+        ... )
+        >>> assert os.path.isfile(os.path.join(temp_directory2.name, 'test/test.txt'))
+        >>> temp_directory1.cleanup()
+        >>> temp_directory2.cleanup()
         """
         destination_file = os.path.abspath(destination_file)
         assert destination_file.endswith('.7z')
-        assert os.path.isdir(os.path.basename(destination_file))
+        assert os.path.isdir(os.path.dirname(destination_file))
+        assert not os.path.isfile(destination_file)
         for f in files:
             assert os.path.isfile(os.path.join(cwd, f))
         output = subprocess.run(
-            ("7z", "a", "-t7z" "-mx=9", "-ms=on", "-md=128m", "-mmt=on", destination_file, *files),
+            ("7z", "a", "-t7z", "-mx=9", "-ms=on", "-md=128m", "-mmt=on", destination_file, *files),
             cwd=cwd,
             capture_output=True,
         )
+        assert os.path.isfile(destination_file)
 
-    def extract(self, cwd, source_file, destination_folder):
+    def extract(self, cwd, source_file, destination_folder='./'):
         assert source_file.endswith('.7z')
         assert os.path.isfile(os.path.join(cwd, source_file))
         assert os.path.isdir(os.path.join(cwd, destination_folder))
         output = subprocess.run(
-            ("7z", "x", source_file, destination_folder),
+            ("7z", "x", source_file, f"-o{destination_folder}"),
             cwd=cwd,
             capture_output=True,
         )
-        assert len(os.scandir(os.path.join(cwd, destination_folder)))
+        assert len(tuple(os.scandir(os.path.abspath(os.path.join(cwd, destination_folder)))))
